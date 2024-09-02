93015154183765faaffd814f0dfe2fe7e05d0001
Rahaf Rashid228 <layarashed619@gmail.com>
2024-09-02 13:50:33 +0300

    creat folder structure

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import 'dotenv/config';
+import initApp from './src/initApp.js';
+
+
+const app= express();
+ 
+initApp(app,express);
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is running on port ${process.env.PORT}!`)
+})
---
+++ src/Modules/auth/auth.router.js
@@ -0,0 +1,11 @@
+import{Router} from 'express';
+const router =Router();
+import * as Authconteroller from './auth.controller.js';
+
+
+
+
+
+
+
+export default router;
